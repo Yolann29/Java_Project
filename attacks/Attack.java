b71decf7f1487ce2840bd35703f2f6feb2a9d168
Victor Morellet <victor.morellet@epitech.eu>
2023-11-07 17:28:18 +0100

    feat(Fighter): add method pickWeapon and necessities

--- attacks/Attack.java
+++ attacks/Attack.java
@@ -1,4 +1,5 @@
 package attacks;
 
 public abstract class Attack {
+    
 }
--- weapons/Weapon.java
+++ weapons/Weapon.java
@@ -1,4 +1,11 @@
 package weapons;
+import types.Type;
+import attacks.Attack;
 
 public abstract class Weapon {
+    protected String name;
+    protected Type type;
+    protected int damage;
+    protected int level;
+    protected Attack[] attacks;
 }
