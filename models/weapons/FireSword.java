54f7c3f4d3b2f8d3df5ab35c67b1859d78472c1f
Victor Morellet <victor.morellet@epitech.eu>
2023-11-14 10:42:08 +0100

    style(Weapons): remove unwanted imports

--- models/weapons/FireSword.java
+++ models/weapons/FireSword.java
@@ -1,10 +1,6 @@
 package models.weapons;
 
 import models.types.Type;
-import models.weapons.attacks.Explosion;
-import models.weapons.attacks.Strike;
-import models.weapons.attacks.Tornado;
-import models.weapons.attacks.Wave;
 
 public class FireSword extends Weapon {
     public FireSword() {
--- models/weapons/IceSword.java
+++ models/weapons/IceSword.java
@@ -1,11 +1,6 @@
 package models.weapons;
 
 import models.types.Type;
-import models.weapons.attacks.Attack;
-import models.weapons.attacks.Strike;
-
-import java.util.List;
-import java.util.Random;
 
 public class IceSword extends Weapon {
 
